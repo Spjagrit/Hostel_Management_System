ee09c38dd4fcaf891850cb70eb0fa9ca0f5d8d88
S P JAGRIT <97080055+Spjagrit@users.noreply.github.com>
2023-05-03 00:37:38 +0530

    Add files via upload

---
+++ admin/includes/sidebar.php
@@ -0,0 +1,24 @@
+<nav class="ts-sidebar">
+			<ul class="ts-sidebar-menu">
+			
+				<li class="ts-label">Main</li>
+				<li><a href="dashboard.php"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+					<li><a href="#"><i class="fa fa-files-o"></i> Courses</a>
+					<ul>
+						<li><a href="add-courses.php">Add Courses</a></li>
+						<li><a href="manage-courses.php">Manage Courses</a></li>
+					</ul>
+				</li>
+					<li><a href="#"><i class="fa fa-desktop"></i> Rooms</a>
+					<ul>
+						<li><a href="create-room.php">Add a Room</a></li>
+						<li><a href="manage-rooms.php">Manage Rooms</a></li>
+					</ul>
+				</li>
+
+				<li><a href="registration.php"><i class="fa fa-user"></i>Student Registration</a></li>
+				<li><a href="manage-students.php"><i class="fa fa-users"></i>Manage Students</a></li>
+				<li><a href="access-log.php"><i class="fa fa-file"></i>User Access logs</a></li>
+
+			
+		</nav>
